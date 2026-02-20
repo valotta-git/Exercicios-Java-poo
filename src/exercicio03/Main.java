f9db503eeaf3ccb4adea68055d6a84c8e6732990
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-19 22:16:01 -0300

    Ex 03

---
+++ src/exercicio03/Main.java
@@ -0,0 +1,22 @@
+package exercicio03;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("#,##0.00");
+        Professor professor = new Professor();
+
+
+        System.out.println("Qual o nome do professor? ");
+        professor.nome = sc.next();
+
+        System.out.println("Q");
+
+
+    }
+}
---
+++ src/exercicio03/Professor.java
@@ -0,0 +1,27 @@
+package exercicio03;
+
+public class Professor {
+
+    String nome;
+    double numAulaSemanal;
+    double valHoraAula;
+
+    public double salBase () {
+
+        return numAulaSemanal * 4.5 * valHoraAula;
+
+    }
+
+    public double AdicionalHora () {
+        return salBase() * 0.05;
+    }
+
+    public double DescansoRemunerado () {
+        return (salBase() + AdicionalHora()) / 6;
+    }
+
+    public double calcularSal () {
+        return salBase() + AdicionalHora() + DescansoRemunerado();
+    }
+
+}
